b104bf898eb550849fe0f3b7072f544b875759d4
snehalupatil <patilsnehal4575@gmail.com>
2021-05-10 20:44:44 +0530

    Added: User need to enter a valid Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,31 +2,50 @@ import java.util.regex.*;
 import java.util.*;
 import java.lang.*;
 
-public class UserRegistration {
+public class UserRegistration
+{
     private static String user;
 
-    public static String inputUserValue() {
+    public static String inputUserValue()
+    {
         Scanner input = new Scanner(System.in);
         user = input.nextLine();
-        return user;
+        return user ;
     }
 
 
     //UC1: "Adding : Check user first name is valid or not "
-    public static void checkfirstName() {
+    public static void checkfirstName()
+    {
         String firstName;
         System.out.println("Enter first Name of user: ");
         firstName = inputUserValue();
-        boolean check_FirstName = Pattern.matches("[A-Z]+[a-z]{3,}", firstName);
-        if (check_FirstName == true)
-            System.out.println("First name is : " + firstName);
+        boolean check_FirstName = Pattern.matches("[A-Z]+[a-z]{3,}",firstName);
+        if(check_FirstName == true)
+            System.out.println("First name is  " +firstName);
         else
             System.out.println("First name is invalid Enter the valid first name ");
     }
 
+    //UC2: "Adding : Check user last name is valid or not "
+    public static void checklastName()
+    {
+        String lastName;
+        System.out.println();
+        System.out.println("Enter Last Name of user ");
+        lastName = inputUserValue();
+        boolean check_LastName = Pattern.matches("[A-Z]+[a-z]{3,}",lastName);
+        if(check_LastName == true)
+            System.out.println("Last name is : " +lastName);
+        else
+            System.out.println("Last name is invalid Enter the valid first name ");
+    }
 
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args)
+    {
         checkfirstName();
-
+        checklastName();
     }
 }
